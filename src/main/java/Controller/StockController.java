b6b99becf260f0e05692e1978f688585802196b2
Rensnauta <144660205+Rensnauta@users.noreply.github.com>
2024-06-02 17:55:11 +0200

    Moved database code from StockController to ProductDAO

--- src/main/java/Controller/StockController.java
+++ src/main/java/Controller/StockController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import DAO.ProductDAO;
 import Model.Product;
 
 import javax.servlet.ServletException;
@@ -7,52 +8,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.apache.tomcat.jdbc.pool.PoolProperties;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 
 @WebServlet(name = "StockController", urlPatterns = {"/stock"})
 public class StockController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PoolProperties p = new PoolProperties();
-        p.setUrl("jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:6543/postgres");
-        p.setDriverClassName("org.postgresql.Driver");
-        p.setUsername("postgres.jsrjkbulxchlrombsxks");
-        p.setPassword("zS83wKIA6qn8zmet");
-
-        DataSource datasource = new DataSource();
-        datasource.setPoolProperties(p);
-
-        Connection con = null;
-        try {
-            con = datasource.getConnection();
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from productdata");
-            while (rs.next()) {
-                String name = rs.getString("productname");
-                int productNr = rs.getInt("productnr");
-                Date expirationDate = rs.getDate("expirationdate");
-                int stock = rs.getInt("stock");
-                String category = rs.getString("category");
-                double price = rs.getDouble("price");
-                System.out.println("data retrieved");
-                new Product(name, productNr, category, expirationDate, stock, price);
-            }
-            rs.close();
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        } finally {
-            if (con!=null) try {con.close();}catch (Exception ignore) {}
-        }
+        ProductDAO.retrieveProducts();
         ArrayList<Product> products = Product.getProducts();
         for (Product product : products) {
             response.getWriter().write("Product: ");
--- src/main/java/DAO/ProductDAO.java
+++ src/main/java/DAO/ProductDAO.java
@@ -1,7 +1,50 @@
 package DAO;
 
 
-public class ProductDAO {
+import Model.Product;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class ProductDAO {
+    public static void retrieveProducts() {
+        PoolProperties p = new PoolProperties();
+        p.setUrl("jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:6543/postgres");
+        p.setDriverClassName("org.postgresql.Driver");
+        p.setUsername("postgres.jsrjkbulxchlrombsxks");
+        p.setPassword("zS83wKIA6qn8zmet");
+
+        DataSource datasource = new DataSource();
+        datasource.setPoolProperties(p);
+
+        Connection con = null;
+        try {
+            con = datasource.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from productdata");
+            while (rs.next()) {
+                String name = rs.getString("productname");
+                int productNr = rs.getInt("productnr");
+                Date expirationDate = rs.getDate("expirationdate");
+                int stock = rs.getInt("stock");
+                String category = rs.getString("category");
+                double price = rs.getDouble("price");
+                System.out.println("data retrieved");
+                new Product(name, productNr, category, expirationDate, stock, price);
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            if (con!=null) try {con.close();}catch (Exception ignore) {}
+        }
+    }
 }
 
