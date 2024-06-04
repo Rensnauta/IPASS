0706aa9633362bcf056829afde9a63858caf7656
Rensnauta <144660205+Rensnauta@users.noreply.github.com>
2024-06-04 15:26:01 +0200

    updated stock and stockcontroller, data is sent to the browser via a fetch

--- Frontend/Scripts/Controller/StockController.js
+++ Frontend/Scripts/Controller/StockController.js
@@ -1,11 +1,17 @@
 const fetchurl = "http://localhost:8080/api/stock"
 
-fetch(fetchurl) 
-.then((Response) => {
-    if(!Response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-    }
-    return Response.array.forEach(element => {
-        console.log(element)
+fetch(fetchurl)
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach(element => {
+            console.log(element)
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
     });
-})
