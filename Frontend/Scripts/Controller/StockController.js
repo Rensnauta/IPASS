9c493e0f3159c40f6735c0c2cfa0152dc219df86
Rensnauta <144660205+Rensnauta@users.noreply.github.com>
2024-06-05 12:59:32 +0200

    implemented view layer in stock screen

--- Frontend/Scripts/Controller/StockController.js
+++ Frontend/Scripts/Controller/StockController.js
@@ -1,41 +1,36 @@
 const fetchurl = "http://localhost:8080/api/stock"
 
 import { Product } from "../Model/Product"
-let allProducts = []
+import { StockView } from "../View/StockView"
+let stockview = new StockView
 
-new Product (12, 3123, "test", 1-1-2032, 12, "vegetables", 12)
+let productData = []
 
-function fetchProducts() {
-    console.log("fetching products")
-    fetch(fetchurl)
-        .then((response) => {
-            if(!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`)
-            }
-            return response.json();
-        })
-        .then(jsonData => {
-            let tableHTML = '<table><tr><th>Productnumber</th><th>Product Name</th><th>Expiration Date</th><th>Stock</th><th>Category</th><th>Price</th></tr>';
-            jsonData.forEach(item => {
-                tableHTML += `<tr>
-                    <td>${item.productNr}</td>
-                    <td>${item.productName}</td>
-                    <td>${item.expirationDate}</td>
-                    <td>${item.stock}</td>
-                    <td>${item.category}</td>
-                    <td>${item.price}</td>
-                </tr>`;
-            });
-            tableHTML += '</table>';
-            document.body.innerHTML = tableHTML;
-        })
-        .catch(error => {
-                console.log("Failed to fetch products:", error)
-        })
+class StockController {
+    fetchProducts() {
+        console.log("fetching products")
+        fetch(fetchurl)
+            .then((response) => {
+                if(!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`)
+                }
+                return response.json();
+            })
+            .then(jsonData => {
+                productData = jsonData
+                stockview.drawTable(productData)
+            })
+            .catch(error => {
+                    console.log("Failed to fetch products:", error)
+            })
+    }
 }
 
 
 
+
+
 window.onload = () => {
-    fetchProducts();
+    const controller = new StockController
+    controller.fetchProducts();
 };  
