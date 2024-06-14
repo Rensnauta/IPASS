defc9056a387b6fad374e0743941cd140d407b27
Rensnauta <144660205+Rensnauta@users.noreply.github.com>
2024-06-14 15:41:49 +0200

    added links that can be used to link to product information page

--- Frontend/Scripts/View/StockView.js
+++ Frontend/Scripts/View/StockView.js
@@ -3,12 +3,12 @@ export class StockView {
         let tableHTML = '<table id = "stockTable"><tr><th>Productnumber</th><th>Product Name</th><th>Expiration Date</th><th>Stock</th><th>Category</th><th>Price</th></tr>';
     products.forEach(item => {
         tableHTML += `<tr>
-            <td data-cell = "productnr">${item.productNr}</td>
-            <td data-cell = "productname">${item.productName}</td>
-            <td data-cell = "expirationdate">${item.expirationDate}</td>
-            <td data-cell = "stock">${item.stock}</td>
-            <td data-cell = "category">${item.category}</td>
-            <td data-cell = "price">${item.price}</td>
+            <td data-cell="productnr"> <a href="homepage.html">${item.productNr}</a></td>
+            <td data-cell="productname">${item.productName}</td>
+            <td data-cell="expirationdate">${item.expirationDate}</td>
+            <td data-cell="stock">${item.stock}</td>
+            <td data-cell="category">${item.category}</td>
+            <td data-cell="price">${item.price}</td>
         </tr>`;
         })
         tableHTML += '</table>';
